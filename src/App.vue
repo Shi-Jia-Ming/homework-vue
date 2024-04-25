245dc6c4087e6f1ec0ba046b6d587f8ea29fac6f
Shi-Jia-Ming <2516844846@qq.com>
2024-04-25 10:46:11 +0800

    fix: Fix global style.

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,16 @@
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
-
 <style lang="scss">
 html,
 body,
-#app,
+div#app,
 .app {
   padding: 0;
   margin: 0;
   width: 100%;
   height: 100%;
+  max-width: 1920px;
+  max-height: 1080px;
 }
 </style>
