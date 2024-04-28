ab99c8b2c71c2343e5e5a02d128f03eff3984490
Shi-Jia-Ming <sjiaming38@gmail.com>
2024-04-28 20:46:26 +0800

    Add username in store.

--- src/store/modules/user.state.ts
+++ src/store/modules/user.state.ts
@@ -2,14 +2,16 @@ import { Module } from "vuex";
 
 export interface UserStateInterface {
   userId: number | null,
-  token: string | null
+  token: string | null,
+  username: string | null
 }
 
 export const userStore: Module<any, UserStateInterface> = {
   namespaced: true,
   state: {
     userId: null as number | null,
-    token: null as string | null
+    token: null as string | null,
+    username: null as string | null
   },
   mutations: {
     setUserId(state: UserStateInterface, userId: number) {
@@ -18,13 +20,18 @@ export const userStore: Module<any, UserStateInterface> = {
     setToken(state: UserStateInterface, token: string) {
       state.token = token;
     },
+    setUsername(state: UserStateInterface, username: string) {
+      state.username = username;
+    },
     clearUser(state: UserStateInterface) {
       state.userId = null;
       state.token = null;
+      state.username = null;
     },
     setUserState(state: UserStateInterface, result: UserStateInterface) {
       state.userId = result.userId;
       state.token = result.token;
+      state.username = result.username;
     }
   }
 }
